43e4b266138062a3930cdcd727516c22df73173f
gatlee-creator <gatlee@highpoint.edu>
2019-09-18 13:15:50 -0400

    added likes to Post model db

--- server/server.py
+++ server/server.py
@@ -17,6 +17,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     comments = db.relationship('Comment',backref='post_owner')
+    likes = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return f"Post({self.id}, {self.content}), {self.comments}"
